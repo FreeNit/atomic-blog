cde605656b55de6704e10f27270312cc864e12f1
Dimon <bolilui.dmytro@gmail.com>
2024-10-24 12:06:55 +0300

    Added memo, useMemo and useCallback for memoization and application performance optimization

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function SlowComponent() {
+function SlowComponent({ children }) {
   // If this is too slow on your maching, reduce the `length`
   const words = Array.from({ length: 100_000 }, () => "WORD");
   return (
@@ -14,13 +14,31 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+  return (
+    <div>
+      <Counter>
+        <SlowComponent />
+      </Counter>
     </div>
   );
 }
